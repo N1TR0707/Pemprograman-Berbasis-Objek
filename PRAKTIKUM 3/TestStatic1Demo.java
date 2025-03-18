4384134691bab3a251de73afcc9da6c762e08447
N1TR0.707 <128132166+N1TR0707@users.noreply.github.com>
2025-03-18 20:06:01 +0700

    Add files via upload

---
+++ PRAKTIKUM 3/TestStatic1Demo.java
@@ -0,0 +1,10 @@
+public class TestStatic1Demo {
+    public static void main(String[] args) {
+        // Memanggil method satu dan dua
+        TestStatic1.satu();
+        TestStatic1.dua();
+
+        // Mencetak atribut
+        TestStatic1.cetakAtribut();
+    }
+}
